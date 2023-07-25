ca17e9fe3dd0cee8356c5be20e55c3cbe25b7a91
Lorenz230 <lorenzcamento0@gmail.com>
2023-07-25 17:48:16 +0100

    completed task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+
+    let total = 0;
+    let min = 0; 
+    let max = 0;
+
+    if(num1 < num2){
+        min = num1;
+        max = num2;
+    }
+    else{
+        min = num2;
+        max = num1;
+    }
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR";
+    }
+
+
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
+
+
+
+    max++;
+    for(i = min; i < max; i++){
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
