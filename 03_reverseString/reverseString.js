288546eae4bc2fc0ac90b7ba9806fb3db8b183c7
Lorenz230 <lorenzcamento0@gmail.com>
2023-07-25 15:41:00 +0100

    completed task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    text = text.split("");
+    text = text.reverse();
+    text = text.join("");
+    return text;
 };
 
 // Do not edit below this line
